439b6c4ebc42ae14884b33676f1eac2ab6e95340
Bart van benthem <bart.vanbenthem@ns.nl>
2020-11-03 16:19:59 +0100

    update vmclient test

--- main.go
+++ main.go
@@ -23,11 +23,13 @@ func main() {
 
 	// TEST THE VM CLIENT
 	var vmclient vm.RmVMClient
+	fmt.Printf("%-20v %-40v %-10v %-40v %v\n", "Name", "workspaceID", "ostype", "UUID", "managedby")
+	fmt.Printf("%-20v %-40v %-10v %-40v %v\n", "----", "-----------", "------", "----", "---------")
 	for _, vm := range vmclient.List(auth, subscriptionID) {
 		workspace := vmclient.GetWorkspaceID(auth, vm.Name, vm.ResourceGroup, vm.SubscriptionID)
 		managedby := vmclient.GetManagedByTag(auth, vm.Name, vm.ResourceGroup, vm.SubscriptionID)
 		ostype := vmclient.GetOSType(auth, vm.Name, vm.ResourceGroup, vm.SubscriptionID)
 		vmid := vmclient.GetVMID(auth, vm.Name, vm.ResourceGroup, vm.SubscriptionID)
-		fmt.Printf("%-20v %-40v %-20v %-10v %v\n", vm.Name, workspace, ostype, vmid, managedby)
+		fmt.Printf("%-20v %-40v %-10v %-40v %v\n", vm.Name, workspace, ostype, vmid, managedby)
 	}
 }
