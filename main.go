e07901cc9c8e8036858cd649dbf5072a603d664f
Bart van benthem <bart.vanbenthem@ns.nl>
2020-11-04 11:14:09 +0100

    uncomment

--- main.go
+++ main.go
@@ -6,25 +6,25 @@ import (
 
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/bartvanbenthem/azure-update-mgmt/law"
+	"github.com/bartvanbenthem/azure-update-mgmt/printer"
 )
 
 func main() {
-	/*
-		// create an authorizer from the following environment variables
-		// AZURE_CLIENT_ID
-		// AZURE_CLIENT_SECRET
-		// AZURE_TENANT_ID
-		rmAuth, err := auth.NewAuthorizerFromEnvironment()
-		if err != nil {
-			panic(err)
-		}
-		// GET AZURE_SUBSCRIPTION_ID
-		subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
 
-		// test Virtual Machine information printer
-		var print printer.PrintClient
-		print.VM(rmAuth, subscriptionID)
-	*/
+	// create an authorizer from the following environment variables
+	// AZURE_CLIENT_ID
+	// AZURE_CLIENT_SECRET
+	// AZURE_TENANT_ID
+	rmAuth, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		panic(err)
+	}
+	// GET AZURE_SUBSCRIPTION_ID
+	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+
+	// test Virtual Machine information printer
+	var print printer.PrintClient
+	print.VM(rmAuth, subscriptionID)
 
 	// test LAW query
 	lawAuth, err := auth.NewAuthorizerFromEnvironmentWithResource("https://api.loganalytics.io")
