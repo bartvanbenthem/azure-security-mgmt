0b93d024ba8f307a324af9a7c82f6bc9bd8f5953
Bart van benthem <bart.vanbenthem@ns.nl>
2020-11-04 22:26:24 +0100

    update

--- law/law.go
+++ law/law.go
@@ -2,17 +2,42 @@ package law
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/services/operationalinsights/v1/operationalinsights"
 	"github.com/Azure/go-autorest/autorest"
 )
 
+type LAWQueryResult struct {
+	Tables []struct {
+		Name    string `json:"name"`
+		Columns []struct {
+			Name string `json:"name"`
+			Type string `json:"type"`
+		} `json:"columns"`
+		Rows [][]interface{} `json:"rows"`
+	} `json:"tables"`
+}
+
 type LAWClient struct{}
 
 func StringToPointer(s string) *string { return &s }
 
-func (c *LAWClient) ResultParser() {}
+func (c *LAWClient) ResultParserByte(qresult operationalinsights.QueryResults) ([]byte, error) {
+	result, err := json.Marshal(qresult)
+	if err != nil {
+		return result, err
+	}
+
+	return result, err
+}
+
+func (c *LAWClient) ResultParserLAWQueryResult(qresult []byte) LAWQueryResult {
+	var law LAWQueryResult
+	json.Unmarshal(qresult, &law)
+	return law
+}
 
 func (c *LAWClient) Query(a autorest.Authorizer, workspaceID, lawQuery string) (operationalinsights.QueryResults, error) {
 	queryClient := operationalinsights.NewQueryClient()
@@ -33,18 +58,6 @@ func (c *LAWClient) Query(a autorest.Authorizer, workspaceID, lawQuery string) (
 }
 
 // TEMP FUNCTIONS FOR TESTING PURPOSES
-
-type Output struct {
-	Tables []struct {
-		Name    string `json:"name"`
-		Columns []struct {
-			Name string `json:"name"`
-			Type string `json:"type"`
-		} `json:"columns"`
-		Rows [][]interface{} `json:"rows"`
-	} `json:"tables"`
-}
-
 type QueryResultRaw struct {
 	Header string
 	Rows   []string
--- main.go
+++ main.go
@@ -6,25 +6,25 @@ import (
 
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/bartvanbenthem/azure-update-mgmt/law"
-	"github.com/bartvanbenthem/azure-update-mgmt/printer"
 )
 
 func main() {
+	/*
+		// create an authorizer from the following environment variables
+		// AZURE_CLIENT_ID
+		// AZURE_CLIENT_SECRET
+		// AZURE_TENANT_ID
+		rmAuth, err := auth.NewAuthorizerFromEnvironment()
+		if err != nil {
+			panic(err)
+		}
+		// GET AZURE_SUBSCRIPTION_ID
+		subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
 
-	// create an authorizer from the following environment variables
-	// AZURE_CLIENT_ID
-	// AZURE_CLIENT_SECRET
-	// AZURE_TENANT_ID
-	rmAuth, err := auth.NewAuthorizerFromEnvironment()
-	if err != nil {
-		panic(err)
-	}
-	// GET AZURE_SUBSCRIPTION_ID
-	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
-
-	// test Virtual Machine information printer
-	var print printer.PrintClient
-	print.VM(rmAuth, subscriptionID)
+		// test Virtual Machine information printer
+		var print printer.PrintClient
+		print.VM(rmAuth, subscriptionID)
+	*/
 
 	// test LAW query
 	lawAuth, err := auth.NewAuthorizerFromEnvironmentWithResource("https://api.loganalytics.io")
@@ -40,10 +40,26 @@ func main() {
 		fmt.Println(err)
 	}
 
-	result := lawclient.ReturnQueryResultCommaSep(qresult)
-	// print comma sepperated results
-	fmt.Println(result.Header)
-	for _, row := range result.Rows {
-		fmt.Printf("%v\n", row)
+	bresult, err := lawclient.ResultParserByte(qresult)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res := lawclient.ResultParserLAWQueryResult(bresult)
+
+	for _, table := range res.Tables {
+		for _, col := range table.Columns {
+			fmt.Printf("%v,", col.Name)
+		}
+		fmt.Printf("\n")
+	}
+
+	for _, table := range res.Tables {
+		for _, row := range table.Rows {
+			for _, item := range row {
+				fmt.Printf("%v,", item)
+			}
+			fmt.Printf("\n")
+		}
 	}
 }
